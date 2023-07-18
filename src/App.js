d010971ddfd98b4efca1e262537ef1cf72ce4f27
Anastasia <www.nastya97@yandex.ru>
2023-07-18 19:55:22 +0300

    feat: get pizzas array from mockApi

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React from 'react';
 import './scss/app.scss';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Sort from './components/Sort';
 import PizzaBlock from './components/PizzaBlock';
-import pizzasArray from './assets/pizzas.json';
+//import pizzasArray from './assets/pizzas.json';
 
+// https://64b59113f3dbab5a95c77af8.mockapi.io/items
 function App() {
+  const [pizzasArray, setPizzasArray] = React.useState([]);
+
+  React.useEffect(() => {
+    fetch('https://64b59113f3dbab5a95c77af8.mockapi.io/items')
+      .then((res) => {
+        return res.json();
+      })
+      .then((arr) => {
+        console.log(arr);
+        setPizzasArray(arr);
+      });
+  }, []);
+
   return (
     <div className="wrapper">
       <Header />
